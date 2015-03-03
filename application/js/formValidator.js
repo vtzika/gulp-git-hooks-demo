2eea9b86e7e3939a86565c4a1d43c3c22d23d314
Maria Cristina Di Termine <mariacristinadt@gmail.com>
2015-03-03 14:54:46 +0100

    Made some javascript and unit testing (string empty, email validation, number validation and number of the letters)

--- application/js/formValidator.js
+++ application/js/formValidator.js
@@ -1,20 +1,40 @@
-var formValidator = (function() {
+var formValidator = (function () {
 
-    var fieldNotEmpty = function(field) {
-        if (field === '') {
+    var fieldNotEmpty = function (field) {
+        if(field==='') {
+            return false;
+        } 
+        return true;
+    } 
+
+    var fieldIsNumber = function (number) {
+    
+        if(isNaN(number)) {
             return false;
         }
         return true;
     }
 
-    var fieldIsNumber = function(number) {
-        if (isNaN(number)) {
-            return false;
+    var fieldValidEmail = function (email) {
+        var emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(emailRegEx.test(email)) {
+            return true;
         }
-        return true;
-    }
+        return false;
+    } 
 
-    return { fieldNotEmpty: fieldNotEmpty,
-             fieldIsNumber: fieldIsNumber
+    var fieldMinLet = function (letters) {
+        if(letters.length > 2) {
+            return true;
+        }
+        return false;
+    }
+    
+    return { fieldNotEmpty : fieldNotEmpty,
+             fieldIsNumber : fieldIsNumber,
+             fieldValidEmail : fieldValidEmail,
+             fieldMinLet : fieldMinLet
      }
+
+
 }());
--- spec/FormvalidatorSpec.js
+++ spec/FormvalidatorSpec.js
@@ -1,7 +1,7 @@
 // Form
-describe ('Formvalidator contains', function() {
+describe ('Formvalidator contains', function () {
     // Not empty
-    describe ('Required field', function() {
+    describe ('Required field', function () {
         it ('should be false when the field is empty', function() {
             var r = formValidator.fieldNotEmpty('');
             expect(r).toBe(false);
@@ -9,19 +9,47 @@ describe ('Formvalidator contains', function() {
         it ('should be true when the field is not empty', function() {
             var r = formValidator.fieldNotEmpty('field');
             expect(r).toBe(true);
-        })
+        });
     });
 
     // Number
-    describe ('Numerical field', function() {
+    describe ('Numerical field', function () {
         it ('should be false when the field is not a number', function() {
             var r = formValidator.fieldIsNumber('number');
             expect(r).toBe(false);
+            
         });
         it ('should be true when the field is a number', function() {
             var r = formValidator.fieldIsNumber(7);
             expect(r).toBe(true);
-        })
-    })
+        });
+    });
+
+    // Email signs
+    describe ('Valid email', function () {
+        it ('should be false when the email is not valid', function() {
+            var r = formValidator.fieldValidEmail('');
+            expect(r).toBe(false);
+            
+        });
+        it ('should be true when the email is valid', function() {
+            var r = formValidator.fieldValidEmail('email@email.com');
+            expect(r).toBe(true);
+        });
+    });
+
+
+    // Minimum number of letters
+    describe ('Minimum number of letters', function () {
+        it ('should be false when there are not enough letters', function() {
+            var r = formValidator.fieldMinLet('a');
+            expect(r).toBe(false);
+            
+        });
+        it ('should be true when there are enough letters', function() {
+            var r = formValidator.fieldMinLet('abc');
+            expect(r).toBe(true);
+        });
+    });
 
 });
